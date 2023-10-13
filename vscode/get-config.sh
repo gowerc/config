674debf019cfbac3ba742d7a395f56c3c42f3499
Craig Gower-Page <craiggower@gmail.com>
2023-10-13 12:32:40 +0100

    Update get-config.sh

--- vscode/get-config.sh
+++ vscode/get-config.sh
@@ -1,14 +1,1 @@
 
-HERE=$PWD/vscode/config
-
-VSPATH="/Users/gowerc/Library/Application Support/Code/User"
-
-code --list-extensions >  $HERE/extensions.txt
-
-cp -r "$VSPATH/keybindings.json"  $HERE/keybindings.json
-cp -r "$VSPATH/settings.json"  $HERE/settings.json
-
-rsync -a --delete  "$VSPATH/snippets/" "$HERE/snippets/"
-
-
-
