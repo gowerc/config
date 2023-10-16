13693f388c8c2d52cd0105f96c00c32584fc39d0
Craig Gower-Page <craiggower@gmail.com>
2023-10-16 20:19:02 +0100

    Update setup_linux.sh

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 
 
-## Bash Profile
-wget -q -O - https://raw.githubusercontent.com/gowerc/config/master/bash/bash_profile  > $HOME/.bash_profile
-wget -q -O - https://raw.githubusercontent.com/gowerc/config/master/bash/bashrc  > $HOME/.bashrc
-wget -q -O - https://raw.githubusercontent.com/gowerc/config/master/bash/profile_common  > $HOME/.profile_common
 
-
-## zshrc
-wget -q -O - https://raw.githubusercontent.com/gowerc/config/master/zsh/.zshrc  > $HOME/.zshrc
-
-## Rstudio 
-mkdir -p ~/.config/rstudio
-wget -q -O - https://raw.githubusercontent.com/gowerc/config/master/rstudio/rstudio-prefs.json  > $HOME/.config/rstudio/rstudio-prefs.json
-
-
-## Vim
-wget -q -O - https://raw.githubusercontent.com/gowerc/config/master/vim/vimrc >  $HOME/.vimrc
 
